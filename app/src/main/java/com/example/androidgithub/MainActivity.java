571de51367e7bbe22dd2980b56628dabd70f83b9
정하령 <87926493+SeongKooJeung@users.noreply.github.com>
2023-04-01 12:51:27 +0900

    commit3 -- 컴퓨터 2 변경

--- app/src/main/java/com/example/androidgithub/MainActivity.java
+++ app/src/main/java/com/example/androidgithub/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //commit1 -- 기능 구현 확인
-        //commit1 -- 기능1 추가
+        //commit2 -- 기능1 추가
+        //commit3 -- 컴퓨터 2 변경
     }
 }
