0c15bdaa360586d7b9de8a5c3cfe5d713f7ca924
정하령 <87926493+SeongKooJeung@users.noreply.github.com>
2023-04-01 12:48:56 +0900

    commit2 -- 기능1 추가

--- app/src/main/java/com/example/androidgithub/MainActivity.java
+++ app/src/main/java/com/example/androidgithub/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //commit1 -- 기능 구현 확인
+        //commit1 -- 기능1 추가
     }
 }
